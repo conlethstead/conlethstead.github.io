93a8585cb096574ce2bc6ff9ba914d7ac2c5ef19
Conleth Stead <cstead@ford.com>
2025-02-13 12:04:22 -0500

    Styles files

--- src/projects/threadLibrary.tsx
+++ src/projects/threadLibrary.tsx
@@ -1,6 +1,8 @@
+import './projectStyles/threadLibrary.css';
+
 export const ThreadLibrary = () => {
     return (
-        <div>
+        <div className="thread-library">
             <h2>Thread Library</h2>
         </div>
     );
