6962047fc947a5a9091b8e8432d76a91b5256ec0
Conleth Stead <cstead@ford.com>
2025-02-13 11:55:37 -0500

    Scrolling working

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     const targetElement = document.getElementById(targetId!);
     const headerOffset = document.querySelector('.nav-header')?.clientHeight || 0;
     const elementPosition = targetElement?.getBoundingClientRect().top || 0;
-    const offsetPosition = elementPosition - headerOffset;
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
 
     window.scrollTo({
       top: offsetPosition,
